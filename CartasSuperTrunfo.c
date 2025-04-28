f5eed506c76017b2ecdabf2458ef65edb278d4a5
Xpl0ds <thiag0sant0s@hotmail.com>
2025-04-28 13:13:36 +0000

    comecando

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,16 +3,20 @@
 int main(){
 
     printf("Desafio Super Trunfo");
-
+    printf("\n");
     char Cidade[20];
-    int Codigo da Cidade;
+    int CodigoDaCidade;
+    int Populacao;
     float Area;
     float PIB;
-    int Numero de pontos turisticos;
+    int NumeroDePontosTuristicos;
 
-    printf("Goiania")
-    scanf("%s", &Cidade)
+    printf("Digite o nome da Cidade: \n");
+    scanf("%s", &Cidade);
+
+    printf("Digite o Codigo da Cidade: \n");
+    scanf("%d", &CodigoDaCidade);
 
 
-
+    return 0;
 }
